08ba820a72f55686b863ddd87d036f21f967f644
yannana123 <1272242183@qq.com>
2022-06-13 00:08:22 +0800

    注册

---
+++ src/components/registerNav.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <van-nav-bar left-arrow @click-left="onClickLeft" />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onClickLeft() {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.van-nav-bar {
+  background-color: #000;
+  color: #fff;
+}
+::v-deep .van-nav-bar .van-icon {
+  color: #aaa;
+}
+[class*="van-hairline"]::after {
+  border: none;
+}
+</style>
